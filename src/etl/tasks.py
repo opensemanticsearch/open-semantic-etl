1d228d32ff35fe24cf70f5eb28b0bedac1fd75b4
Markus Mandalka <github@mandalka.name>
2017-03-15 08:06:11 +0100

    Add additional tasks for other ETL types so API can add them to queue

--- src/etl/tasks.py
+++ src/etl/tasks.py
@@ -2,30 +2,28 @@
 # -*- coding: utf-8 -*-
 
 #
-# Files queue for batch processing and parallel processing
+# Queue tasks for batch processing and parallel processing
 #
 
 # Queue handler
 from celery import Celery
 
 # ETL connectors
-from etl_file import Connector_File
+from etl import ETL
 from etl_delete import Delete
+from etl_file import Connector_File
+from etl_web import Connector_Web
+from etl_rss import Connector_RSS
 
+
+verbose = False
 
 app = Celery('tasks')
 
-connector_etl_file = Connector_File()
-connector_etl_delete = Delete()
-
-
-#
-# Index a file
-#
-
-@app.task
-def index_file(filename):
-	connector_etl_file.index(filename=filename)
+etl_delete = Delete()
+etl_file = Connector_File()
+etl_web = Connector_Web()
+etl_rss = Connector_RSS()
 
 
 #
@@ -34,7 +32,83 @@ def index_file(filename):
 
 @app.task
 def delete(uri):
-	connector_etl_delete.delete(uri=uri)
+	etl_delete.delete(uri=uri)
+
+
+#
+# Index a file
+#
+
+@app.task
+def index_file(filename, wait=0):
+
+	if wait:
+		time.sleep(wait)
+
+	etl_file.index(filename=filename)
+
+
+#
+# Index a webpage
+#
+
+@app.task
+def index_web(uri, wait=0):
+
+	if wait:
+		time.sleep(wait)
+
+	result = etl_web.index(uri)
+
+	return result
+
+
+#
+# Index RSS Feed
+#
+
+@app.task
+def index_rss(uri, wait=0):
+
+	if wait:
+		time.sleep(wait)
+
+	result = etl_rss.index(uri)
+
+	return result
+
+
+
+#
+# Enrich with / run plugins
+#
+
+@app.task
+def enrich(plugins, uri, wait=0):
+	
+	if wait:
+		time.sleep(wait)
+	
+	etl = ETL()
+	etl.read_configfile('/etc/opensemanticsearch/etl')
+	etl.read_configfile('/etc/opensemanticsearch/enhancer-rdf')
+	
+	etl.config['plugins'] = plugins.split(',')
+
+	filename = uri
+
+	# if exist delete protocoll prefix file://
+	if filename.startswith("file://"):
+		filename = filename.replace("file://", '', 1)
+	
+	parameters = etl.config.copy()
+			
+	parameters['id'] = uri
+	parameters['filename'] = filename
+	
+	parameters, data = etl.process (parameters=parameters, data={})
+
+	return data
 	
 
 #
@@ -46,38 +120,20 @@ if __name__ == "__main__":
 
 	from optparse import OptionParser 
 
-	#get uri or filename from args
-
 	parser = OptionParser("etl-tasks [options]")
 	parser.add_option("-q", "--quiet", dest="quiet", action="store_true", default=None, help="Don\'t print status (filenames) while indexing")
 	parser.add_option("-v", "--verbose", dest="verbose", action="store_true", default=None, help="Print debug messages")
-	parser.add_option("-f", "--force", dest="force", action="store_true", default=None, help="Force (re)indexing, even if no changes")
-	parser.add_option("-c", "--config", dest="config", default=False, help="Config file")
-	parser.add_option("-p", "--plugins", dest="plugins", default=False, help="Plugins (comma separated and in order)")
 
 	(options, args) = parser.parse_args()
 
-	connector_etl_file.read_configfile ('/etc/etl/config')
-	connector_etl_delete.read_configfile ('/etc/etl/config')
-
-	# add optional config parameters
-	if options.config:
-		connector_etl_file.read_configfile(options.config)
-		connector_etl_delete.read_configfile(options.config)
-
-	# set (or if config overwrite) plugin config
-	if options.plugins:
-		connector_etl_file.config['plugins'] = options.plugins.split(',')
-
 	if options.verbose == False or options.verbose==True:
-		connector_etl_file.verbose = options.verbose
-		connector_etl_delete.verbose = options.verbose
+		verbose = options.verbose
+		etl_delete.verbose = options.verbose
+		etl_file.verbose = options.verbose
+		etl_web.verbose = options.verbose
+		etl_rss.verbose = options.verbose
 		
 	if options.quiet == False or options.quiet==True:
-		connector_etl_file.quiet = options.quiet
+		etl_file.quiet = options.quiet
 
-	if options.force == False or options.force==True:
-		connector_etl_file.config['force'] = options.force
-
-	
 	app.worker_main()
