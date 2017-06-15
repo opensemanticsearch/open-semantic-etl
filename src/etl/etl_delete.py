53a0545ec374414383f8f13216ed4e687b7c0648
Markus Mandalka <github@mandalka.name>
2017-06-15 20:25:29 +0200

    Command line tool / option to empty the index

--- src/etl/etl_delete.py
+++ src/etl/etl_delete.py
@@ -18,6 +18,14 @@ class Delete(ETL):
 		
 		self.read_configfiles()
 
+			# read on what DB or search server software our index is
+		export = self.config['export']
+	
+		# call delete function of the configured exporter
+		module = importlib.import_module(export)
+		objectreference = getattr(module, export)
+		self.connector = objectreference()
+
 
 	def set_configdefaults(self):
 		#
@@ -31,8 +39,6 @@ class Delete(ETL):
 		self.config['force'] = False
 
 	
-
-
 	def read_configfiles(self):
 		#
 		# include configs
@@ -40,8 +46,7 @@ class Delete(ETL):
 		
 		# Windows style filenames
 		self.read_configfile ('conf\\opensemanticsearch-etl')
-		
-		
+			
 		# Linux style filenames
 		self.read_configfile ('/etc/opensemanticsearch/etl')
 		
@@ -50,16 +55,16 @@ class Delete(ETL):
 
 		if self.verbose:
 			print "Deleting from index {}".format(uri)
-		# read on what DB or search server software our index is
-		export = self.config['export']
-	
-		# call delete function of the configured exporter
-		module = importlib.import_module(export)
-		objectreference = getattr(module, export)
-		connector = objectreference()
-	
-		connector.delete(parameters=self.config, docid=uri)
+		
+		self.connector.delete(parameters=self.config, docid=uri)
 
+
+	def empty(self):
+
+		if self.verbose:
+			print "Deleting all documents from index"
+	
+		self.connector.delete(parameters=self.config, query="*:*")
 		
 
 #
@@ -74,12 +79,14 @@ if __name__ == "__main__":
 	#get uri or filename from args
 
 	parser = OptionParser("etl-delete [options] URI(s)")
+	parser.add_option("-e", "--empty", dest="empty", action="store_true", default=False, help="Empty the index (delete all documents in index)")
 	parser.add_option("-v", "--verbose", dest="verbose", action="store_true", default=None, help="Print debug messages")
 	parser.add_option("-c", "--config", dest="config", default=False, help="Config file")
 
 	(options, args) = parser.parse_args()
 
-	if len(args) < 1:
+
+	if not options.empty and len(args) < 1:
 		parser.error("No URI given")
 
 	connector = Delete()
@@ -90,17 +97,13 @@ if __name__ == "__main__":
 	if options.config:
 		connector.read_configfile(options.config)
 
-	if options.outputfile:
-		connector.config['outputfile'] = options.outputfile
-
-	# set (or if config overwrite) plugin config
-	if options.plugins:
-		connector.config['plugins'] = options.plugins.split(',')
-
 	if options.verbose == False or options.verbose==True:
 		connector.verbose = options.verbose
-		
+
+	if options.empty:
+		connector.empty()
+				
 	# index each filename
 	for uri in args:
-		connector.delte(uri)
+		connector.delete(uri)
 
