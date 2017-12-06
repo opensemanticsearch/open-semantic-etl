a4f7eb2ece361bc0c384bfe8629b0369c5a037e7
Hillar <hillar@users.noreply.github.com>
2017-12-06 15:49:06 +0200

    skip empty words as hashtags

--- src/opensemanticetl/enhance_extract_hashtags.py
+++ src/opensemanticetl/enhance_extract_hashtags.py
@@ -3,7 +3,9 @@ class enhance_extract_hashtags(object):
 
 	def process (self, parameters={}, data={} ):
 
-		data['hashtag_ss'] = [ word for word in data['content'].split() if word.startswith("#") ]
+		minimallenght = 3
+		
+		data['hashtag_ss'] = [ word for word in data['content'].split() if (word.startswith("#") and len(word) > minimallenght) ]
 	
 		return parameters, data
 		
