f2679e5a4f91a1a8a22f7456d6b612e4e64a80f6
Markus Mandalka <github@mandalka.name>
2017-12-14 13:46:47 +0100

    Add support for Italian and Czech grammar/stemming

--- src/opensemanticetl/enhance_multilingual.py
+++ src/opensemanticetl/enhance_multilingual.py
@@ -11,7 +11,7 @@ class enhance_multilingual(object):
 	verbose = False
 
 	# languages that are defined in index schema for language specific analysis and used if autodetected as documents language
-	languages = ['en','fr','de','es','ru','nl','pl','ro','pt']
+	languages = ['en','fr','de','es','ru','nl','pl','ro','pt','it','cz']
 	# todo: add all dynamic language field types preconfigured in Solr
 
 	# languages for language specific analysis even if not the autodetected document language
