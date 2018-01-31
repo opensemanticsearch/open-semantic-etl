d8edb1ca099274654d3b6c71da4251e383305047
Markus Mandalka <github@mandalka.name>
2018-01-31 17:42:08 +0100

    For better performance on indexing RDF knowledge graphs index all RDF triples of an entity/subject at once

--- src/opensemanticetl/enhance_rdf.py
+++ src/opensemanticetl/enhance_rdf.py
@@ -1,6 +1,15 @@
 import sys
+import logging
 import rdflib
+from rdflib import Graph
+from rdflib import RDFS
+from rdflib import Namespace
 
+# define used ontologies / standards / properties
+skos = Namespace('http://www.w3.org/2004/02/skos/core#')
+owl = Namespace('http://www.w3.org/2002/07/owl#')
+
+import etl
 from etl import ETL
 
 
@@ -13,168 +22,228 @@ class enhance_rdf(object):
 
 		self.verbose = verbose
 
-	
-	# since we want full text search we want not to use ID/URI but all labels for indexing
-	def get_labels_from_rdfobject(self, obj):
+		self.labelProperties = (rdflib.term.URIRef(u'http://www.w3.org/2004/02/skos/core#prefLabel'), rdflib.term.URIRef(u'http://www.w3.org/2000/01/rdf-schema#label'), rdflib.term.URIRef(u'http://www.w3.org/2004/02/skos/core#altLabel'), rdflib.term.URIRef(u'http://www.w3.org/2004/02/skos/core#hiddenLabel'))
 
-		# if type not literal but URI use label(s) of this Identifier/URI instead of pure URI
+
+	# In which facet to write the predicate / property ?
+	def property2facet(self, prop):
+
+		facet = prop + '_ss'
+
+		return facet
+
+	#
+	# get all labels, alternate labels / synonyms for the URI/subject, if not there, use subject (=URI) as default
+	#
+
+	def get_labels(self, subject):
+
+		labels = []
+	
+		# append RDFS.label
+
+		# get all labels for this obj
+		for label in self.graph.objects(subject=subject, predicate=rdflib.RDFS.label):
+			labels.append(label)
+
+		#
+		# append SKOS labels
+		#
+			
+		# append SKOS prefLabel
+		skos = rdflib.Namespace('http://www.w3.org/2004/02/skos/core#')
+		for label in self.graph.objects(subject=subject, predicate=skos['prefLabel']):
+			labels.append(label)
+
+		# append SKOS altLabels
+		for label in self.graph.objects(subject=subject, predicate=skos['altLabel']):
+			labels.append(label)
+
+		# append SKOS hiddenLabels
+		for label in self.graph.objects(subject=subject, predicate=skos['hiddenLabel']):
+			labels.append(label)
+
+		return labels
+
+	
+	#
+	# Get indexable full text(s) / label(s) instead of URI references
+	#
+
+	def get_values(self, obj):
+
+		values = []
+
+		# since we want full text search we want not to use ID/URI but all labels for indexing
+		# if type not literal but URI reference, add label(s)
+
 		if type(obj) == rdflib.URIRef:
 
-			values = []
+			# get labels of this object (therefore it is a subject for getlabels)
+			values = self.get_labels(subject=obj)
 
-			# append RDFS.label
-
-			# get all labels for this obj
-			for label in self.graph.objects(subject=obj, predicate=rdflib.RDFS.label):
-				values.append( label )
+			if not values: 
 
 				if self.verbose:
-					print ( "Appending RDFS:label: {}".format(label) )
-
-			#
-			# append SKOS labels
-			#
-			
-			# append SKOS prefLabel
-			skos = rdflib.Namespace('http://www.w3.org/2004/02/skos/core#')
-			for label in self.graph.objects(subject=obj, predicate=skos['prefLabel']):
-				values.append( label )
-
-				if self.verbose:
-					print ( "Appending SKOS:prefLabel: {}".format(label) )
-
-			# append SKOS altLabels
-			for label in self.graph.objects(subject=obj, predicate=skos['altLabel']):
-				values.append( label )
-
-				if self.verbose:
-					print ( "Appending SKOS:altLabel: {}".format(label) )
-
-			# append SKOS hiddenLabels
-			for label in self.graph.objects(subject=obj, predicate=skos['hiddenLabel']):
-				values.append( label )
-
-				if self.verbose:
-					print ( "Appending SKOS:hiddenLabel: {}".format(label) )
-
-
-			#if no label use URI instead
-			if len(values) == 0:
-			
-				if self.verbose:
-					print ( "No label, using URI instead: {}".format(obj) )
+					print ( "No label for this object, using URI {}".format(obj) )
+		
 				values = obj
 
-			# (maybe) todo:
-			# because if not in file but available:
-			# get label from external graph(s) or triplestore
+		elif type(obj) == rdflib.term.Literal:
+				values = obj
 
-		else:
-			
+		# if no values or labels, use the object / URI
+		if not values:
 			if self.verbose:
-				print ( "Data type of value (RDF object): {}".format(type(obj)) )
-				print ( "Using its value {}".format(obj) )
-			
-			values = obj
+				print ( "No label or URI for this object, using object {}".format(obj) )
+				print ( "Data type of RDF object: {}".format(type(obj)) )
 
+			values = obj
 
 		return values
 
 
-	def	etl_graph(self, parameters):
-	
-		# Print infos
+
+	# best/preferred label as title
+	def get_preferred_label(self, subject, lang='en'):
+		
+			preferred_label = self.graph.preferredLabel(subject=subject, lang=lang, labelProperties=self.labelProperties)
+
+			# if no label in preferred language, try with english, if not preferred lang is english yet)
+			if not preferred_label and not lang == 'en':
+				
+				preferred_label = self.graph.preferredLabel(subject=subject, lang='en', labelProperties=self.labelProperties)
+
+			# use label from some other language
+			if not preferred_label:
+				
+				preferred_label = self.graph.preferredLabel(subject=subject, lang='en', labelProperties=self.labelProperties)
+
+			# if no label, use URI
+			if preferred_label:
+				# since return is tuple with type and label take only the label
+				preferred_label = preferred_label[0][1]
+			else:
+				preferred_label = subject
+
+			return preferred_label
+
+
+	#
+	# ETL knowledge graph to full text search index
+	#
+
+	# Index each entity / subject with all its properties/predicates as facets and objects (dereference URIs by their labels) as values
+
+
+	def etl_graph(self, parameters):
+
 		if self.verbose:
 			print("Graph has {} triples.".format(len(self.graph)) )
 	
-		count = 0
+		count_triple = 0
+		count_subjects = 0
 	
 		part_parameters = {}
-
 		part_parameters['plugins'] = []
-		
-		# todo like enhance_path for properties & subjects?
-		# abstract variable of enhance_path plugin?
-		
 		part_parameters['export'] = parameters['export']
 
-
+		etl_processor = ETL()
+		etl_processor.verbose = self.verbose
+		
+		
 		# since there can be multiple triples/values for same property,
 		# do not overwrite document but add value to existent document & values of the facet/field/property
 		part_parameters['add'] = True
 		# but not for the field content_type which doesn't change and is not multi valued
 		part_parameters['fields_set'] = "content_type"
 
-		for subj, pred, obj in self.graph:
+		# use SPARQL query with distinct to get subjects only once
+		res = self.graph.query(
+			"""SELECT DISTINCT ?subject
+			WHERE {
+			?subject ?predicate ?object .
+			}""")
+	
+		for row in res:
+
+			count_subjects += 1
+	
+			if self.verbose:
+				print( "Importing entity / subject {}".format(count_subjects) )
+
+			# get subject of the concept from first column
+			subj = row[0]
+
+			if self.verbose:
+				print ( "Processing RDF subject {}".format(subj) )
 
 			part_data = {}
 			part_data['content_type'] = 'Knowledge graph'
+			# subject as URI/ID
+			part_parameters['id'] = subj
+			part_data['title'] = self.get_preferred_label(subject=subj)
 
-			count += 1
 
-			if self.verbose:
-				print( "Importing triple {}".format(count) )
+			count_subject_triple = 0
 
-			try:					
-	
-				# subject as URI/ID
-				part_parameters['id'] = subj
+			# get all triples for this subject
+			for pred, obj in self.graph.predicate_objects(subject=subj):
+
+				count_triple += 1
+				count_subject_triple += 1
 
 				if self.verbose:
-					print ( "ID (RDF subject): {}".format(subj) )
+					print( "Importing subjects triple {}".format(count_subject_triple) )
+					print( "Predicate / property: {}".format(pred) )
+					print( "Object / value: {}".format(obj) )
 
-				#
-				# Predicate/property to facet/field
-				#
-				rdf_property = pred
 
-				# set Solr datatype so facets not available yet in Solr schema can be inserted automatically (dynamic fields) with right datatype
-				facet = rdf_property + '_ss'
+				try:
+					#
+					# Predicate/property to facet/field
+					#
+
+					# set Solr datatype strings so facets not available yet in Solr schema can be inserted automatically (dynamic fields) with right datatype
+					
+					facet = self.property2facet(prop=pred)
+
+					if self.verbose:
+						print ( "Facet: {}".format(facet) )
+
 	
-				if self.verbose:
-					print ( "Facet: {}".format(facet) )
-				#
-				# object to facet/field value
-				#
+					#
+					# get values or labels of this object
+					#
 
-				value = self.get_labels_from_rdfobject(obj)						
-				
-				# insert or append value (object of triple) to data
-				part_data[facet] = value
+					values = self.get_values(obj=obj)
 
-				#
-				# Property statistics
-				#
-				
-				# add to facet property where you can see which properties are available
-				part_data['property_ss'] = pred
+					if self.verbose:
+						print ( "Values: {}".format(values) )
 
 
-				# todo: set parameter to add instead of update for multiple triples/values for/with same property 
+					# insert or append value (object of triple) to data
+					etl.append(data=part_data, facet=facet, values=values)
+
+				except KeyboardInterrupt:
+					raise KeyboardInterrupt
 	
-				etl = ETL()
-				
-				etl.verbose = self.verbose
-	
+				except BaseException as e:
+					sys.stderr.write( "Exception while triple {} of subject {}: {}\n".format(count_subject_triple, subj, e) )
 
-				# index triple
-				etl.process( part_parameters, part_data)
+
 	
-			except KeyboardInterrupt:
-				raise KeyboardInterrupt
 	
-			except BaseException as e:
-				sys.stderr.write( "Exception while triple {}: {}\n".format(count, e) )
+			# index triple
+			etl_processor.process( part_parameters, part_data)
 	
 
-	def	etl_graph_file(self, docid, filename, parameters={}):
+	def etl_graph_file(self, docid, filename, parameters={}):
 		
 		self.graph = rdflib.Graph()
 		self.graph.parse(filename)
 
 		self.etl_graph(parameters=parameters)
-
-	
 
 
 	def process (self, parameters={}, data={} ):
