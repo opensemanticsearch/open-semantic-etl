13c04151b064180dd5d3dc1ad7fcb841d2b3d934
Markus Mandalka <github@mandalka.name>
2018-04-27 16:20:56 +0200

    Use variable type suffix for fieldnames to use Solr default schema

--- src/opensemanticetl/etl_sparql.py
+++ src/opensemanticetl/etl_sparql.py
@@ -93,6 +93,7 @@ class Connector_SPARQL(ETL):
 		
 		# linux style filenames
 		self.read_configfile ('/etc/opensemanticsearch/etl')
+		self.read_configfile ('/etc/opensemanticsearch/etl-custom')
 		self.read_configfile ('/etc/opensemanticsearch/enhancer-rdf')
 		self.read_configfile ('/etc/opensemanticsearch/connector-sparql')
 		
