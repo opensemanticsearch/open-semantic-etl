9d4434ef93406701f6fe524f728314ecd41469e0
Markus Mandalka <github@mandalka.name>
2018-11-26 11:21:15 +0100

    Entity Linking / automatic tagging: Add taxonomy structure

--- src/opensemanticetl/enhance_entity_linking.py
+++ src/opensemanticetl/enhance_entity_linking.py
@@ -1,12 +1,48 @@
+#
+# Named Entity Extraction by Open Semantic Entity Search API dictionary
+#
+
+
 import requests
 import json
 import etl
 
 from entity_linking.entity_linker import Entity_Linker
 
+
 #
-# Named Entity Extraction by Open Semantic Entity Search API dictionary
+# split a taxonomy entry to separated index fields
 #
+def taxonomy2fields(field, data, separator="\t", field_suffix="_ss"):
+	
+	result = {}
+	
+	# if not multivalued field, convert to used list/array strucutre
+	if not isinstance(data, list):
+		data = [data]
+	
+	for taxonomy_entry in data:
+	
+		i = 0
+		path = ''
+		for taxonomy_entry_part in taxonomy_entry.split(separator):
+
+			taxonomy_fieldname = field + str(i) + field_suffix
+			
+			if not taxonomy_fieldname in result:
+				result[taxonomy_fieldname] = []
+			
+			if len(path)>0:
+				path += separator
+
+			path += taxonomy_entry_part
+			
+			result[taxonomy_fieldname].append(path)
+			
+			i += 1
+	
+	return result
+
 
 class enhance_entity_linking(object):
 
@@ -36,6 +72,8 @@ class enhance_entity_linking(object):
 		if 'openrefine_server' in parameters:
 			openrefine_server = parameters['openrefine_server']
 
+		taxonomy_fields = ['skos_broader_taxonomy_prefLabel_ss']
+
 		text = ''
 		for field in data:
 			
@@ -59,7 +97,7 @@ class enhance_entity_linking(object):
 			linker = Entity_Linker()
 			linker.verbose = verbose
 
-			results = linker.entities( text = text, taggers = entity_linking_taggers )
+			results = linker.entities( text = text, taggers = entity_linking_taggers, additional_result_fields = taxonomy_fields )
 			
 		if verbose:
 			print ("Named Entity Linking: {}".format(results))
@@ -70,6 +108,13 @@ class enhance_entity_linking(object):
 					for facet in candidate['type']:
 						etl.append(data, facet, candidate['name'])
 						etl.append(data, facet + '_uri_ss', candidate['id'])
+						
+						for taxonomy_field in taxonomy_fields:
+							if taxonomy_field in candidate:
+								separated_taxonomy_fields = taxonomy2fields(field=facet, data=candidate[taxonomy_field])
+								for separated_taxonomy_field in separated_taxonomy_fields:
+									etl.append(data, separated_taxonomy_field, separated_taxonomy_fields[separated_taxonomy_field])
+
 
 		# mark the document, that it was analyzed by this plugin yet
 		data['enhance_entity_linking_b'] = "true"
