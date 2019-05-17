52629501bc67b4b6b3325d9508af7fb9022319c7
Markus Mandalka <github@mandalka.name>
2019-05-17 13:38:13 +0200

    More comments on task priorization / Plugins that should be runned later after all standard plugins runned

--- src/opensemanticetl/export_queue_files.py
+++ src/opensemanticetl/export_queue_files.py
@@ -13,11 +13,12 @@ class export_queue_files(object):
 	
 	def process (self, parameters={}, data={} ):
 
-		# write to queue
+		# add file to ETL queue with standard priorizazion
+		# but don't if only plugins not runned that should be runned later with lower priority (which will be added to queue in step below)
 		if not 'only_additional_plugins_later' in parameters:
 			index_file.apply_async( kwargs={ 'filename': parameters['filename'] }, queue='tasks', priority=5 )
 
-		# write to (lower priorized) queue with additional plugins
+		# add file to (lower priorized) ETL queue with additional plugins which should be runned later after all files tasks of standard priorized queue done
 		# to run ETL of the file later again with additional plugins like OCR which need much time/resources while meantime all files are searchable by other plugins which need fewer resources
 		if 'additional_plugins_later' in parameters:
 			index_file.apply_async( kwargs={ 'filename': parameters['filename'], 'additional_plugins': parameters['additional_plugins_later'] }, queue='tasks', priority=1 )
