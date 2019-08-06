64579ecac2cc20f898dd42d166e369edaccce789
Markus Mandalka <github@mandalka.name>
2019-08-06 23:43:48 +0200

    Code quality: Automated unit test for plugin enhance_pdf_pcr.py

---
+++ src/opensemanticetl/test_enhance_pdf_ocr.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import unittest
+
+import enhance_pdf_ocr
+
+class Test_enhance_pdf_ocr(unittest.TestCase):
+
+    # check OCR of embedded images in PDF
+    def test_pdf_ocr(self):
+
+        enhancer = enhance_pdf_ocr.enhance_pdf_ocr()
+
+        parameters = {'filename': 'test/test.pdf', 'content_type_ss': 'application/pdf','plugins':[]}
+
+        parameters, data = enhancer.process(parameters=parameters)
+
+        self.assertTrue('TestPDFOCRImage1Content1' in data['ocr_t'])
+        self.assertTrue('TestPDFOCRImage1Content2' in data['ocr_t'])
+        self.assertTrue('TestPDFOCRImage2Content1' in data['ocr_t'])
+        self.assertTrue('TestPDFOCRImage2Content2' in data['ocr_t'])
+
+    # check OCR after descewing with Scantailor of embedded images in PDF
+    def test_pdf_ocr_descew(self):
+
+        enhancer = enhance_pdf_ocr.enhance_pdf_ocr()
+
+        parameters = {'filename': 'test/test.pdf', 'content_type_ss': 'application/pdf','plugins':['enhance_ocr_descew']}
+
+        parameters, data = enhancer.process(parameters=parameters)
+        self.assertTrue('TestPDFOCRImage1' in data['ocr_descew_t'])
+        self.assertTrue('TestPDFOCRImage2' in data['ocr_descew_t'])
+
+       
+if __name__ == '__main__':
+    unittest.main()
+
