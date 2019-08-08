640753bf4163977f0e6595a48347276689884595
Markus Mandalka <github@mandalka.name>
2019-08-08 13:59:58 +0200

    Code quality: Automated unit test for mapping ids like directories or urls

---
+++ src/opensemanticetl/test_enhance_mapping_id.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import unittest
+
+import enhance_mapping_id
+
+class Test_enhance_mapping_id(unittest.TestCase):
+
+    def test(self):
+
+        enhancer = enhance_mapping_id.enhance_mapping_id()
+
+        mappings = {
+                       "/": "file:///",
+                       "/testdir1/": "file:///deep1testdir1/",
+                       "/testdir1/testdir2/": "file:///deep2testdir1/deep2testdir2/",
+        }
+        
+        docid = '/test'
+        parameters, data = enhancer.process(parameters={'id': docid, 'mappings': mappings})
+        self.assertEqual(parameters['id'], 'file:///test')
+
+        docid = '/testdir1/test'
+        parameters, data = enhancer.process(parameters={'id': docid, 'mappings': mappings})
+        self.assertEqual(parameters['id'], 'file:///deep1testdir1/test')
+
+        docid = '/testdir1/testdir2/test'
+        parameters, data = enhancer.process(parameters={'id': docid, 'mappings': mappings})
+        self.assertEqual(parameters['id'], 'file:///deep2testdir1/deep2testdir2/test')
+
+       
+if __name__ == '__main__':
+    unittest.main()
+
