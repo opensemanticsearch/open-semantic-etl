313adca1a042082be4711d22bb047f28644224ed
Markus Mandalka <github@mandalka.name>
2020-01-16 11:05:37 +0100

    Unittest for plugin enhance_extract_email: Extend to multiline and some special chars

--- src/opensemanticetl/test_enhance_extract_email.py
+++ src/opensemanticetl/test_enhance_extract_email.py
@@ -12,13 +12,15 @@ class Test_enhance_extract_email(unittest.TestCase):
         enhancer = enhance_extract_email.enhance_extract_email()
 
         data = {}
-        data['content_txt'] = 'one@localnet.localdomain at begin and two@localnet2.localdomain in the middle and last three@localnet3.localdomain'
+        data['content_txt'] = "one@localnet.localdomain at begin and two@localnet2.localdomain in the middle and end of the line three@localnet3.localdomain\na_underscore@localnet.localdomain and some.points.here@localnet.localdomain"
 
         parameters, data = enhancer.process(data=data)
 
         self.assertTrue('one@localnet.localdomain' in data['email_ss'])
         self.assertTrue('two@localnet2.localdomain' in data['email_ss'])
         self.assertTrue('three@localnet3.localdomain' in data['email_ss'])
+        self.assertTrue('a_underscore@localnet.localdomain' in data['email_ss'])
+        self.assertTrue('some.points.here@localnet.localdomain' in data['email_ss'])
 
         self.assertTrue('localnet.localdomain' in data['email_domain_ss'])
         self.assertTrue('localnet2.localdomain' in data['email_domain_ss'])
