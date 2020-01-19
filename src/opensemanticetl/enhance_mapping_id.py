6c9f344a095ead8327b3ce6dd98c8e7a97b1ffc5
Markus Mandalka <github@mandalka.name>
2020-01-19 12:56:48 +0100

    Mapping ids: Reverse function

--- src/opensemanticetl/enhance_mapping_id.py
+++ src/opensemanticetl/enhance_mapping_id.py
@@ -26,7 +26,7 @@ def mapping(value, mappings=None):
     if mapping is None:
         mappings = {}
 
-    best_match_count = -1
+    max_match_len = -1
 
     # check all mappings for matching and use the best
     for map_from, map_to in mappings.items():
@@ -35,15 +35,43 @@ def mapping(value, mappings=None):
         if value.startswith(map_from):
 
             # if from string longer (deeper path), this is the better matching
-            match_count = len(map_from)
+            match_len = len(map_from)
 
-            if match_count > best_match_count:
-                best_match_count = match_count
+            if match_len > max_match_len:
+                max_match_len = match_len
                 best_match_map_from = map_from
                 best_match_map_to = map_to
 
     # if there is a match, replace first occurance of value with mapping
-    if best_match_count >= 0:
+    if max_match_len >= 0:
         value = value.replace(best_match_map_from, best_match_map_to, 1)
+
+    return value
+
+
+# Change mapped value to origin value
+def mapping_reverse(value, mappings=None):
+    if mapping is None:
+        mappings = {}
+
+    max_match_len = -1
+
+    # check all mappings for matching and use the best
+    for map_from, map_to in mappings.items():
+
+        # map from matching value?
+        if value.startswith(map_to):
+
+            # if from string longer (deeper path), this is the better matching
+            match_len = len(map_to)
+
+            if match_len > max_match_len:
+                max_match_len = match_len
+                best_match_map_from = map_from
+                best_match_map_to = map_to
+
+    # if there is a match, replace first occurance of value with reverse mapping
+    if max_match_len >= 0:
+        value = value.replace(best_match_map_to, best_match_map_from, 1)
 
     return value
--- src/opensemanticetl/test_enhance_mapping_id.py
+++ src/opensemanticetl/test_enhance_mapping_id.py
@@ -29,6 +29,27 @@ class Test_enhance_mapping_id(unittest.TestCase):
         parameters, data = enhancer.process(parameters={'id': docid, 'mappings': mappings})
         self.assertEqual(parameters['id'], 'file:///deep2testdir1/deep2testdir2/test')
 
+
+    def test_reverse(self):
+
+        mappings = {
+                       "/": "file:///",
+                       "/testdir1/": "file:///deep1testdir1/",
+                       "/testdir1/testdir2/": "file:///deep2testdir1/deep2testdir2/",
+        }
+        
+        docid = 'file:///test'
+        reversed_value = enhance_mapping_id.mapping_reverse (docid, mappings)
+        self.assertEqual(reversed_value, '/test')
+
+        docid = 'file:///deep1testdir1/test'
+        reversed_value = enhance_mapping_id.mapping_reverse (docid, mappings)
+        self.assertEqual(reversed_value, '/testdir1/test')
+
+        docid = 'file:///deep2testdir1/deep2testdir2/test'
+        reversed_value = enhance_mapping_id.mapping_reverse (docid, mappings)
+        self.assertEqual(reversed_value, '/testdir1/testdir2/test')
+
        
 if __name__ == '__main__':
     unittest.main()
