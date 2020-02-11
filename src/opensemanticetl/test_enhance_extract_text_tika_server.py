c26582b71b72e6a10830e39c02aa87b25b253405
Markus Mandalka <github@mandalka.name>
2020-02-11 18:11:03 +0100

    Unittest for Tika Plugin: Add OCR tests

--- src/opensemanticetl/test_enhance_extract_text_tika_server.py
+++ src/opensemanticetl/test_enhance_extract_text_tika_server.py
@@ -27,6 +27,31 @@ class Test_enhance_extract_text_tika_server(unittest.TestCase):
         self.assertTrue('TestPDFContent2 on TestPDFPage2' in data['content_txt'])
 
 
+    def test_text_extraction_pdf_ocr(self):
+
+        enhancer = enhance_extract_text_tika_server.enhance_extract_text_tika_server()
+
+        parameters = {'ocr': True, 'filename': os.path.dirname(os.path.realpath(__file__)) + '/test/test.pdf'}
+
+        parameters, data = enhancer.process(parameters=parameters)
+
+        # check extracted content type
+        self.assertTrue(data['content_type_ss'] == ['application/pdf', 'image/jpeg', 'image/png'])
+
+        # check extracted title
+        self.assertEqual(data['title_txt'], 'TestPDFtitle')
+
+        # check extracted content of PDF text
+        self.assertTrue('TestPDFContent1 on TestPDFPage1' in data['content_txt'])
+        self.assertTrue('TestPDFContent2 on TestPDFPage2' in data['content_txt'])
+
+        # check OCR of embedded images in PDF (result of plugin enhance_pdf_ocr.py)
+        self.assertTrue('TestPDFOCRImage1Content1' in data['content_txt'])
+        self.assertTrue('TestPDFOCRImage1Content2' in data['content_txt'])
+        self.assertTrue('TestPDFOCRImage2Content1' in data['content_txt'])
+        self.assertTrue('TestPDFOCRImage2Content2' in data['content_txt'])
+
+
     def test_ocr_png(self):
 
         enhancer = enhance_extract_text_tika_server.enhance_extract_text_tika_server()
@@ -77,7 +102,10 @@ class Test_enhance_extract_text_tika_server(unittest.TestCase):
         self.assertFalse('TestOCRImage1Content1' in data['content_txt'])
         self.assertFalse('TestOCRImage1Content2' in data['content_txt'])
 
+        # check if Fake tesseract wrapper returned status
+        self.assertTrue('[Image (no OCR yet)]' in data['content_txt'])
        
+
 if __name__ == '__main__':
     unittest.main()
 
