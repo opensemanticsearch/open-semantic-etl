cc888d0023199946e320bf50fa06b43fd2aa2825
Markus Mandalka <github@mandalka.name>
2021-12-27 08:44:03 +0100

    Upgrade unittest to new OCR setup by migration to Tika 2.x where we can not set fake ocr cache by Tika HTTP api header anymore (#142)

--- src/opensemanticetl/test_enhance_extract_text_tika_server.py
+++ src/opensemanticetl/test_enhance_extract_text_tika_server.py
@@ -6,8 +6,24 @@ import os
 
 import enhance_extract_text_tika_server
 
-
 class TestEnhanceExtractTextTikaServer(unittest.TestCase):
+
+    # delete OCR cache entries for the images used in this test class
+    def delete_ocr_cache_entries(self):
+        filenames = [
+            '/var/cache/tesseract/eng-4c6bf51d4455e1cb58b7d8dd20fb8846f15a3d2c884dc8859802ed689f74ae7a-e96c4b1545a83d86d05f7fbb12ade96d.txt',
+            '/var/cache/tesseract/eng-526959d31f4e6b1947bb00c3a02959ef008ce19b9487d95b3df0656159f55a7a-e96c4b1545a83d86d05f7fbb12ade96d.txt',
+            '/var/cache/tesseract/eng-c93c49c9dfc9764a4307c2757eb378b2d8cd00f3007ac450605b83f23ecda900-e96c4b1545a83d86d05f7fbb12ade96d.txt',
+            '/var/cache/tesseract/eng-ebce8ee4ea7d3d24fe9384212d944adeb58e8f18be15ec06103454f7eade70f5-e96c4b1545a83d86d05f7fbb12ade96d.txt'
+        ]
+        for filename in filenames:
+            if os.path.exists(filename):
+                os.remove(filename)
+
+    def setUp(self):
+        self.delete_ocr_cache_entries()
+    def tearDown(self):
+        self.delete_ocr_cache_entries()
 
     def test_text_extraction_pdf(self):
 
@@ -28,7 +44,7 @@ class TestEnhanceExtractTextTikaServer(unittest.TestCase):
         self.assertTrue('TestPDFContent1 on TestPDFPage1' in data['content_txt'])
         self.assertTrue('TestPDFContent2 on TestPDFPage2' in data['content_txt'])
 
-        # check disabled OCR of embedded images in PDF (result of plugin enhance_pdf_ocr.py)
+        # check disabled OCR of embedded images in PDF
         self.assertFalse('TestPDFOCRImage1Content1' in data['content_txt'])
         self.assertFalse('TestPDFOCRImage1Content2' in data['content_txt'])
         self.assertFalse('TestPDFOCRImage2Content1' in data['content_txt'])
@@ -99,9 +115,6 @@ class TestEnhanceExtractTextTikaServer(unittest.TestCase):
                       'filename': os.path.dirname(os.path.realpath(__file__)) + '/test/Test_OCR_Image1.png'}
 
         parameters, data = enhancer.process(parameters=parameters)
-
-        if 'content_txt' not in data:
-            data['content_txt'] = "Empty"
 
         # check extracted content type
         self.assertEqual(data['content_type_ss'], 'image/png')
