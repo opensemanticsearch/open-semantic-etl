828b3c506c222c3c03b243fdeafa8d395cf48db1
Markus Mandalka <github@mandalka.name>
2021-12-27 09:13:04 +0100

    Test if PDF ocr using tesseract-ocr-cache if images once processed (#142)

--- src/opensemanticetl/test_enhance_extract_text_tika_server.py
+++ src/opensemanticetl/test_enhance_extract_text_tika_server.py
@@ -69,11 +69,45 @@ class TestEnhanceExtractTextTikaServer(unittest.TestCase):
         self.assertTrue('TestPDFContent1 on TestPDFPage1' in data['content_txt'])
         self.assertTrue('TestPDFContent2 on TestPDFPage2' in data['content_txt'])
 
-        # check OCR of embedded images in PDF (result of plugin enhance_pdf_ocr.py)
+        # check OCR of embedded images in PDF
         self.assertTrue('TestPDFOCRImage1Content1' in data['content_txt'])
         self.assertTrue('TestPDFOCRImage1Content2' in data['content_txt'])
         self.assertTrue('TestPDFOCRImage2Content1' in data['content_txt'])
         self.assertTrue('TestPDFOCRImage2Content2' in data['content_txt'])
+
+    def test_text_extraction_pdf_ocr_cache(self):
+
+        # add text (changed for this test) to ocr cache, so we can prove that the cache was used
+        file = open('/var/cache/tesseract/eng-c93c49c9dfc9764a4307c2757eb378b2d8cd00f3007ac450605b83f23ecda900-e96c4b1545a83d86d05f7fbb12ade96d.txt', "w")
+        file.write("TestPDFOCRCacheImage1Content1\n\nTestPDFOCRCacheImage1Content2")
+        file.close()
+
+        file = open('/var/cache/tesseract/eng-526959d31f4e6b1947bb00c3a02959ef008ce19b9487d95b3df0656159f55a7a-e96c4b1545a83d86d05f7fbb12ade96d.txt', "w")
+        file.write("TestPDFOCRCacheImage2Content1\n\nTestPDFOCRCacheImage2Content2")
+        file.close()
+
+        enhancer = enhance_extract_text_tika_server.enhance_extract_text_tika_server()
+
+        parameters = {'ocr': True, 'plugins': ['enhance_pdf_ocr'],
+                      'filename': os.path.dirname(os.path.realpath(__file__)) + '/test/test.pdf'}
+
+        parameters, data = enhancer.process(parameters=parameters)
+
+        # check extracted content type
+        self.assertTrue(sorted(data['content_type_ss']) == ['application/pdf', 'image/jpeg', 'image/png'])
+
+        # check extracted title
+        self.assertEqual(data['title_txt'], 'TestPDFtitle')
+
+        # check extracted content of PDF text
+        self.assertTrue('TestPDFContent1 on TestPDFPage1' in data['content_txt'])
+        self.assertTrue('TestPDFContent2 on TestPDFPage2' in data['content_txt'])
+
+        # check OCR of embedded images in PDF
+        self.assertTrue('TestPDFOCRCacheImage1Content1' in data['content_txt'])
+        self.assertTrue('TestPDFOCRCacheImage1Content2' in data['content_txt'])
+        self.assertTrue('TestPDFOCRCacheImage2Content1' in data['content_txt'])
+        self.assertTrue('TestPDFOCRCacheImage2Content2' in data['content_txt'])
 
     def test_ocr_png(self):
 
